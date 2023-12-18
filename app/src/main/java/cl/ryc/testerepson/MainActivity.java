b49eefb717af82b61b5fb4092f6c8f3056125d93
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2023-12-18 18:50:56 -0300

    avances en el ticket de coopeuch

--- app/src/main/java/cl/ryc/testerepson/MainActivity.java
+++ app/src/main/java/cl/ryc/testerepson/MainActivity.java
@@ -1,5 +1,12 @@
 package cl.ryc.testerepson;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.VectorDrawable;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
@@ -7,17 +14,23 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import com.epson.epos2.Epos2CallbackCode;
 import com.epson.epos2.Epos2Exception;
 import com.epson.epos2.printer.Printer;
+import com.epson.epos2.printer.PrinterStatusInfo;
+import com.epson.epos2.printer.ReceiveListener;
+import com.epson.eposprint.Print;
 
 import java.util.HashMap;
 
 import cl.ryc.testerepson.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ReceiveListener {
 
     private ActivityMainBinding binding = null;
     Button btnConnect, btnDisconnect;
@@ -30,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean detected = false;
     private boolean connected = false;
+
+    private static final String ACTION_USB_PERMISSION =
+            "cl.ryc.testerepson.USB_PERMISSION";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,11 +63,12 @@ public class MainActivity extends AppCompatActivity {
         String numeroAtencion = "p22";
         String fila = "Ejecutivo Persona";
 
-        HashMap parameters = new HashMap();
-        parameters.put("sFecha", fecha);
-        parameters.put("sNumeroAtencion", numeroAtencion);
-        parameters.put("sFila", fila);
 
+        //Registrar listener-
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        registerReceiver(usbListener, filter);
 
         mUsbDevice = getIntent().getParcelableExtra(UsbManager.EXTRA_DEVICE);
         if (mUsbDevice != null) {
@@ -59,6 +76,8 @@ public class MainActivity extends AppCompatActivity {
             Log.d("DEVICE_USB", mUsbDevice.toString());
 
             tvPrintName.setText("Impresora detectada: " + mUsbDevice.getProductName());
+
+            Toast.makeText(getApplicationContext(), "Conectado: " + mUsbDevice.getProductName(), Toast.LENGTH_SHORT).show();
 
             if (mUsbDevice.getProductName() != null && mUsbDevice.getProductName().contains("TM-T88V")) {
                 Log.d("PRINTER", "TM-T88V detectada");
@@ -97,6 +116,8 @@ public class MainActivity extends AppCompatActivity {
                     btnDisconnect.setVisibility(View.VISIBLE);
                     btnDisconnect.setEnabled(true);
                     btnConnect.setEnabled(false);
+                    binding.btnPrint.setEnabled(true);
+
 
                     mPrinter.startMonitor();
 
@@ -116,6 +137,7 @@ public class MainActivity extends AppCompatActivity {
                     mPrinter = null;
 
                     btnConnect.setEnabled(true);
+
                     btnDisconnect.setVisibility(View.GONE);
                     btnDisconnect.setEnabled(false);
 
@@ -125,6 +147,103 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+
+        binding.btnPrint.setOnClickListener(v -> {
+
+            Bitmap resizedBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.coopeuch_android);
+
+            if (mPrinter != null) {
+                try {
+                    mPrinter.addTextAlign(Printer.ALIGN_CENTER);
+                    mPrinter.addTextFont(Printer.FONT_A);
+                    mPrinter.addTextSmooth(Printer.TRUE);
+
+                    mPrinter.addImage(resizedBitmap, 0, 0,
+                            resizedBitmap.getWidth(),
+                            resizedBitmap.getHeight(),
+                            Printer.COLOR_1,
+                            Printer.MODE_MONO,
+                            Printer.HALFTONE_DITHER,
+                            Printer.PARAM_DEFAULT,
+                            Printer.COMPRESS_NONE);
+
+                    //SPACING
+                    mPrinter.addFeedLine(1);
+
+                    //Bienvenido
+                    mPrinter.addTextStyle(Print.PARAM_DEFAULT, Printer.PARAM_DEFAULT, Printer.TRUE, Printer.PARAM_DEFAULT);
+
+                    mPrinter.addTextSize(2, 2);
+                    mPrinter.addText("¡Bienvenido!");
+
+                    //SPACING
+                    mPrinter.addFeedLine(3);
+
+                    //Tu numero de atencion
+                    mPrinter.addTextSize(1, 1);
+                    mPrinter.addText("Tu número de atención");
+
+                    //SPACING
+                    mPrinter.addFeedLine(3);
+
+                    //CODIGO NUMERO
+                    mPrinter.addTextSize(5, 5);
+                    mPrinter.addText("C17");
+
+                    //SPACING
+                    mPrinter.addFeedLine(3);
+
+                    //Branch
+                    mPrinter.addTextSize(1, 1);
+                    mPrinter.addText("Caja");
+                    mPrinter.addFeedLine(1);
+                    mPrinter.addText("Conoce el estado de tu ticket \n escaneando este código");
+
+                    //SPACING
+                    mPrinter.addFeedLine(3);
+
+                    //BIENVENIDO
+                    mPrinter.addTextStyle(Print.PARAM_DEFAULT, Printer.PARAM_DEFAULT, Printer.FALSE, Printer.PARAM_DEFAULT);
+                    mPrinter.addText("BIENVENIDO A COOPEUCH");
+                    mPrinter.addFeedLine(5);
+
+                    //FINAL CUT
+                    mPrinter.addFeedLine(3);
+                    mPrinter.addCut(Printer.CUT_FEED);
+
+                    mPrinter.sendData(Printer.PARAM_DEFAULT);
+
+                    mPrinter.clearCommandBuffer();
+                } catch (Epos2Exception e) {
+                    e.printStackTrace();
+                    Log.e("PRINTER", "Error al imprimir el texto: " + e.getMessage() + ", " + e.getErrorStatus());
+                }
+            }
+        });
+    }
+
+
+    public void onPtrReceive(final Printer printerObj, final int code, final PrinterStatusInfo status,
+                             final String printJobId) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public synchronized void run() {
+                if (code == Epos2CallbackCode.CODE_SUCCESS) {
+//Displays successful print messages
+                    Toast.makeText(getApplicationContext(), "Print success", Toast.LENGTH_SHORT).show();
+                } else {
+//Displays error messages
+                    Toast.makeText(getApplicationContext(), "Print failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        new Thread(new Runnable() {
+            @Override
+            public synchronized void run() {
+//Abort process
+            }
+        }).start();
     }
 
     private String makeStatusMassage(int type) {
@@ -161,10 +280,28 @@ public class MainActivity extends AppCompatActivity {
         return msg;
     }
 
+    BroadcastReceiver usbListener = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
+
+                UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                if (device != null) {
+
+                    Log.w("DEVICE_USB", device.toString());
+
+                    Toast.makeText(getApplicationContext(), "Desconectado: " + device.getProductName(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    };
+
     public void onDestroy() {
         if (mPrinter != null) {
             try {
                 mPrinter.disconnect();
+                mPrinter.clearCommandBuffer();
 
                 mPrinter.setStatusChangeEventListener(null);
                 mPrinter = null;
